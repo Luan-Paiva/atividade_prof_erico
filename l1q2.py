fc17b402bb00cc4543ced8b3a2156e3209c73ee3
Luan-Paiva <hjtjtrhjjhth@gmail.com>
2023-03-06 10:47:41 -0300

    primeiro envio

---
+++ l1q2.py
@@ -0,0 +1,8 @@
+#Faça um programa então mostre a mensagem
+# O número foi [número].
+input ("Informe um numero: ")
+
+numero = (input('Informe um número: ')) #entrada de dados
+
+
+print = numero ('O numero informado foi:') #saida de da
